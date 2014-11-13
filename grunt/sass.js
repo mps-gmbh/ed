0b9c3c0fcb17e72b6ce82d5093490e161e6c4884
Sebastian Sebald <sebastian.sebald@gmail.com>
2014-11-13 09:29:28 +0100

    chore(sass): Use `bourbon` via `node-bourbon`.
    - Add node-bourbon
    - Remove manually installed lib

--- grunt/sass.js
+++ grunt/sass.js
@@ -1,10 +1,31 @@
-module.exports =  {
-	options: {
-		sourceMap: true
-	},
-	dev: {
-		files: {
-			'<%= dir.dist %>/<%= package.name %>.css': '<%= dir.src %>/<%= package.name %>.scss'
+module.exports =  function () {
+
+	var main = "@import 'scss/lib/bourbon/bourbon';\n" +
+				"@import 'scss/lib/normalize';\n" +
+
+
+				"@import 'scss/core/font';\n" +
+				"@import 'scss/core/colors';\n" +
+
+				"@import 'scss/core/root';\n" +
+				"@import 'scss/core/animations';\n" +
+				"@import 'scss/core/utils';\n" +
+
+
+				"@import 'scss/components/page';\n" +
+				"@import 'scss/components/brand';\n" +
+				"@import 'scss/components/loading-spinner';\n";
+
+
+	return {
+		options: {
+			sourceMap: true,
+			includePaths: require('node-bourbon').includePaths
+		},
+		dev: {
+			files: {
+				'<%= dir.dist %>/<%= package.name %>.css': '<%= dir.src %>/<%= package.name %>.scss'
+			}
 		}
-	}
+	};
 };
