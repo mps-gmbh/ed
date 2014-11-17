4736613e51330fa4dd818c110abf11482b738d88
Sebastian Sebald <sebastian.sebald@gmail.com>
2014-11-17 13:19:21 +0100

    tests(fixture): Add a fixture for Gihub API responses.

---
+++ src/components/github/github.fixture.js
@@ -0,0 +1,54 @@
+(function () {
+
+	angular.module('github.fixture', [])
+		.service('GithubFixture',
+			function ( GH_FIXTURE_MILESTONES_1, GH_FIXTURE_ISSUES_1, GH_FIXTURE_ISSUES_2 ) {
+
+			this.milestones = GH_FIXTURE_MILESTONES_1;
+			this.issues = {
+				'1' : GH_FIXTURE_ISSUES_1,
+				'2' : GH_FIXTURE_ISSUES_2
+			};
+		})
+
+		// Milestones
+		// -------------------------
+		.value('GH_FIXTURE_MILESTONES_1', [{
+			title: 'Back to the Future',
+			number: 1
+		}, {
+			title: 'Spaceballs',
+			number: 2
+		}])
+
+
+		// Issues
+		// -------------------------
+		.value('GH_FIXTURE_ISSUES_1', [{
+			title: 'The flux capacitor is leaking.',
+			number: 10,
+			state: 'open'
+		}, {
+			title: 'Marty McFly just broke the time barrier.',
+			number: 11,
+			state: 'closed'
+		}, {
+			title: 'Nobody calls me chicken!',
+			number: 12,
+			state: 'open',
+			pull_request: {
+				html_url: 'http://www.mcfly.io'
+			}
+		}])
+
+		.value('GH_FIXTURE_ISSUES_2', [{
+			title: 'What\'s the matter, Colonel Sandurz? Chicken?',
+			number: 20,
+			state: 'open'
+		}, {
+			title: 'They\'ve gone to plaid!',
+			number: 21,
+			state: 'open'
+		}]);
+
+})();
